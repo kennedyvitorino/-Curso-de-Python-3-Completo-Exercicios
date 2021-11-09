4aac29c8edc80959e3e740047e45cb2108b37ce6
Kennedy <elpessimista@gmail.com>
2021-11-09 00:27:03 -0300

    Leia o nome de uma pessoa

--- Exercício #001 - Ola Mundo.py
+++ Exercício #001 - Ola Mundo.py
@@ -1,5 +1,25 @@
 """Faça um programa que leia o nome de uma pessoa
 e mostre uma mensagem de boas-vindas."""
 
-name = str(input('Digite seu nome: '))
-print(f"Olá {name}, Seja muito bem vindo ao » Curso em Vídeo «")
+
+#
+# print('Olá, mundo!')
+#
+# name = str(input('Digite seu nome: '))
+# print(f"Olá {name}, Seja muito bem vindo ao » Curso em Vídeo «")
+# Class
+# Syntaxe
+
+
+class Pessoa:
+    def __init__(self, nome=None, msg=None):
+        self.msg = msg
+        self.nome = nome
+
+    def mensagem(self):
+        self.nome = input('Qual é o seu nome? ')
+        print(f'Olá, {self.nome}! \nSeja bem vindo ao curso')
+
+
+saida = Pessoa()
+saida.mensagem()
