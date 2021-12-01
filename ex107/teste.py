d61d3b83c8f47e9a62c840fdd4029d07d8a9913f
Kennedy <elpessimista@gmail.com>
2021-12-01 19:33:03 -0300

    "adicionado potenciação"

--- ex107/teste.py
+++ ex107/teste.py
@@ -2,15 +2,19 @@
 funções incorporadas aumentar(), diminuir(), dobro() e metade().
 Faça também um programa que importe esse módulo e use algumas dessas funções."""
 import moeda
+from linha import *
 
-valor = float(input('\033[1;91m+----------+----------+----------+----------+\033[m\n'
-                    '\033[91m|\033[m     Por favor, informe o preço: R$'))
+print(linha())
 
-print(f"""\033[91m|----------+----------+----------+----------+
-\033[91m|\033[m  A metade de R${valor} é R${moeda.metade(valor):.2f}
-\033[91m|\033[m  O dobro de R${valor} é R${moeda.dobro(valor):.2f} 
-\033[91m|\033[m  {valor:.2f} aumentado em 10% é R${moeda.aumentar(valor, 10):.2f}
-\033[91m|\033[m  {valor:.2f} diminuido em 13% é R${moeda.diminuir(valor, 13):.2f}
-\033[91m|\033[m  {valor:.2f} vezes ele mesmo é igual à R${moeda.potencia(valor):.2f}
+valor = float(input('          Por favor, informe o preço: R$'))
 
+cabecalho('CONFIRA O RESULTADO LOGO ABAIXO.')
+
+print(f"""{linha()}
+  A metade de R${valor} é R${moeda.metade(valor):.2f}
+  O dobro de R${valor} é R${moeda.dobro(valor):.2f} 
+  {valor:.2f} aumentado em 10% é R${moeda.aumentar(valor, 10):.2f}
+  {valor:.2f} diminuido em 13% é R${moeda.diminuir(valor, 13):.2f}
+  {valor:.2f} vezes ele mesmo é igual à R${moeda.potencia(valor):.2f}
 """)
+
