92ed05184deb7b0a6f23f520e4bb0c22ab630488
Kennedy <elpessimista@gmail.com>
2021-12-01 18:52:16 -0300

    "adicionado def potencia"

--- ex107/moeda.py
+++ ex107/moeda.py
@@ -1,7 +1,6 @@
 """Crie um módulo chamado moeda.py que tenha as
 funções incorporadas aumentar(), diminuir(), dobro() e metade().
 Faça também um programa que importe esse módulo e use algumas dessas funções."""
-
 # funçoes usadas para criação do MODULO
 
 
