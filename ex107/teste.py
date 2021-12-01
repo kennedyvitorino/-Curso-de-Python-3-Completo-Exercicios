1262e76821b644512e10bf80e37be7ffff750ef9
Kennedy <elpessimista@gmail.com>
2021-12-01 19:38:09 -0300

    "adicionado potenciação"

--- ex107/teste.py
+++ ex107/teste.py
@@ -8,7 +8,7 @@ print(linha())
 
 valor = float(input('          Por favor, informe o preço: R$'))
 
-cabecalho('CONFIRA O RESULTADO LOGO ABAIXO.')
+cabecalho('CONFIRA O RESULTADO LOGO ABAIXO')
 
 print(f"""{linha()}
   A metade de R${valor} é R${moeda.metade(valor):.2f}
@@ -16,4 +16,5 @@ print(f"""{linha()}
   {valor:.2f} aumentado em 10% é R${moeda.aumentar(valor, 10):.2f}
   {valor:.2f} diminuido em 13% é R${moeda.diminuir(valor, 13):.2f}
   {valor:.2f} vezes ele mesmo é igual à R${moeda.potencia(valor):.2f}
+{linha()}
 """)
